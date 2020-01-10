2f19e4953ee45326b7eb718e1b19b449ad65a81a
luca <luca.all1996@gmail.com>
2020-01-10 09:23:58 +0100

    Fixed tests

--- variability/_test.py
+++ variability/_test.py
@@ -4,14 +4,16 @@ from main import compute_variant_variability, compute_edit_distance_variability
 from pm4py.algo.filtering.log.variants import variants_filter
 import os
 
+RESOURCE_FOLDER = "./resources/"
 
-@pytest.mark.parametrize("log_name", os.listdir("./resources"))
+
+@pytest.mark.parametrize("log_name", os.listdir(RESOURCE_FOLDER))
 def test_variant_variability(log_name):
-    log = xes_import_factory.apply(log_name)
+    log = xes_import_factory.apply(RESOURCE_FOLDER+log_name)
     assert compute_variant_variability(log) == len(variants_filter.get_variants(log))
 
 
 def test_edit_distance_variability():
-    log_path = "./resources/L3.xes"
+    log_path = RESOURCE_FOLDER + "L3.xes"
     log = xes_import_factory.apply(log_path)
     assert compute_edit_distance_variability(log) == float(5.833333333333333)
