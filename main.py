b4dd7abad90b7df01d596359ef4d5cc9bed37bf9
luca <luca.all1996@gmail.com>
2020-01-24 16:02:03 +0100

    Maybe finished

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pm4py.objects.log.importer.xes import factory as xes_import_factory
 from variability import *
 
 if __name__ == "__main__":
-    log_path = 'resources/exercise2.xes'
+    log_path = 'resources/L3.xes'
     log = xes_import_factory.apply(log_path)
     print(Counter(tuple(event["concept:name"] for event in case) for case in log))
     print(compute_variant_variability(log))
