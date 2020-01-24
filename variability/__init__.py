b175a2191c37e62f29f177e444ff3921042a09df
luca <luca.all1996@gmail.com>
2020-01-24 16:02:03 +0100

    Maybe finished

--- variability/__init__.py
+++ variability/__init__.py
@@ -37,7 +37,11 @@ def compute_edit_distance_variability(log: lg.EventLog) -> float:
         log (EventLog): The log to examine
 
     Returns:
-        float: the average edit distance between each pairs of traces
+        float: a number between 0 and 1 included. The higher the number the
+            more similar the traces are
+            - 0 : if all traces has nothing in common one another
+            - 1 : all traces belongs to the same variant (have the same
+                  sequence of events)
     """
     # Create a dictionary which contains variants as keys and its number of
     # occurrences as values
@@ -52,6 +56,49 @@ def compute_edit_distance_variability(log: lg.EventLog) -> float:
     # In the end we sum all of them.
     sum_of_distances = sum(
         num_of_items_1 * num_of_items_2 * editdistance.eval(variant1, variant2)
+        for (variant1, num_of_items_1), (variant2, num_of_items_2) in combinations(
+            variants_and_counts.items(), 2
+        )
+    )
+
+    # We multiply the sum of distances by 2 because to add the sum of distances
+    # of each inverted pairs of variants.
+    # In the end we divide it by the number of possible combination of pair
+    # of traces
+    return float(sum_of_distances * 2) / (size_of_log * (size_of_log - 1))
+
+
+def compute_my_variability(log: lg.EventLog):
+    """Compute the average of normalized edit distances (Levenshtein distance)
+    between each pairs of traces.
+
+    This function uses function `eval` of module 'editdistance' because it's
+    implemented in C++ and it is faster than the corresponding implementation
+    in python.
+
+    Args:
+        log (EventLog): The log to examine
+
+    Returns:
+        float: a number between 0 and 1 included. The higher the number the
+            more similar the traces are
+            - 0 : if all traces has nothing in common
+            - 1 : all traces belongs to the same variant (are equals)
+    """
+    # Create a dictionary which contains variants as keys and its number of
+    # occurrences as values
+    variants_and_counts = Counter(
+        tuple(event["concept:name"] for event in case) for case in log
+    )
+    size_of_log = len(log)
+
+    # For each pair of distinct variants (obtained by 'combinations') we
+    # compute the average edit distance normalized (divided by longest trace)
+    # and we multiply it for number of occurrences of the variants.
+    # In the end we sum all of them.
+    sum_of_distances = sum(
+        float(num_of_items_1 * num_of_items_2 * editdistance.eval(variant1, variant2))
+        / max(len(variant1), len(variant2))
         for (variant1, num_of_items_1), (variant2, num_of_items_2)
             in combinations(variants_and_counts.items(), 2)
     )
@@ -60,12 +107,25 @@ def compute_edit_distance_variability(log: lg.EventLog) -> float:
     # of each inverted pairs of variants
     # In the end we divide it by the number of possible combination of pair
     # of traces
-    return float(sum_of_distances * 2) / (size_of_log * (size_of_log - 1))
+    return 1 - (sum_of_distances * 2 / (size_of_log * (size_of_log - 1)))
 
 
-def compute_my_variability(log: lg.EventLog):
-    # Create a dictionary which contains variants as keys and its number of
-    # occurrences as values
+'''
+def compute_my_variability_old(log: lg.EventLog):
+    """Compute the sum of edit distances (Levenshtein distance) between each
+    pairs of traces divided by the sum of the maximum edit distances between
+    each pair of traces.
+
+    This function uses function `eval` of module 'editdistance' because it's
+    implemented in C++ and it is faster than the corresponding implementation
+    in python.
+
+    Args:
+        log (EventLog): The log to examine
+
+    Returns:
+        float: a number between 0 and 1 included.
+    """
     variants_and_counts = Counter(
         tuple(event["concept:name"] for event in case) for case in log
     )
@@ -73,213 +133,20 @@ def compute_my_variability(log: lg.EventLog):
     sum_of_distances = 0
     sum_of_max_distances = 0
     for (variant1, num_of_items_1), (variant2, num_of_items_2) in combinations(
-        variants_and_counts.items(), 2
+            variants_and_counts.items(), 2
     ):
         sum_of_distances += (
-            num_of_items_1 * num_of_items_2 * editdistance.eval(variant1, variant2) * 2
+                num_of_items_1 * num_of_items_2 * editdistance.eval(variant1, variant2) * 2
         )
         sum_of_max_distances += (
-            num_of_items_1 * num_of_items_2 * max(len(variant1), len(variant2)) * 2
+                num_of_items_1 * num_of_items_2 * max(len(variant1), len(variant2)) * 2
         )
 
+    # Here we add to max distances the case of picking the traces that belong
+    # to the same variant, so we its length times the number of times it can
+    # occur
     sum_of_max_distances += sum(
         len(k) * v * (v - 1) for k, v in variants_and_counts.items()
     )
     return 1 - (float(sum_of_distances) / sum_of_max_distances)
-
-
-if __name__ == "__main__":
-    s1 = (
-        "assumption laboratory",
-        "assumption laboratory",
-        "assumption laboratory",
-        "unconjugated bilirubin",
-        "bilirubin - total",
-        "glucose",
-        "urea",
-        "hemoglobin photoelectric",
-        "creatinine",
-        "alkaline phosphatase-kinetic-",
-        "sodium flame photometry",
-        "potassium potentiometrically",
-        "SGOT - Asat kinetic",
-        "SGPT - alat kinetic",
-        "Milk acid dehydrogenase LDH kinetic",
-        "ABO blood group and rhesus factor",
-        "rhesus factor d - Centrifuge method - email",
-        "leukocyte count electronic",
-        "platelet count - Electronic",
-        "gammaglutamyltranspeptidase",
-        "CEA - tumor marker using meia",
-        "calcium",
-        "albumin",
-        "red cell antibody screening",
-        "ca-125 using meia",
-        "differentiation leukocytes - manual",
-        "order rate",
-        "First outpatient consultation",
-        "administrative fee - the first pol",
-        "thorax",
-        "thorax",
-        "ct abdomen",
-        "ct abdomen",
-        "demurrage - all spec.beh.kinderg.-Reval.",
-        "assumption laboratory",
-        "assumption laboratory",
-        "assumption laboratory",
-        "urea",
-        "hemoglobin photoelectric",
-        "creatinine",
-        "sodium flame photometry",
-        "potassium potentiometrically",
-        "ABO blood group and rhesus factor",
-        "rhesus factor d - Centrifuge method - email",
-        "differential count automatically",
-        "leukocyte count electronic",
-        "platelet count - Electronic",
-        "red cell antibody screening",
-        "order rate",
-        "190021 clinical admission A002",
-        "190205 Class 3b A205",
-        "190101 reg.toesl above. A101",
-        "demurrage - all spec.beh.kinderg.-Reval.",
-        "ovarian - ovarian redebulking CarCine",
-        "immuno-pathology",
-        "immuno-pathology",
-        "cytology - ascites -",
-        "histological examination - big resectiep",
-        "assumption laboratory",
-        "assumption laboratory",
-        "assumption laboratory",
-        "glucose",
-        "glucose",
-        "methemoglobin - sulphemoglobin each",
-        "hemoglobin photoelectric",
-        "hemoglobin photoelectric",
-        "bicarbonate",
-        "Co-hb kwn.",
-        "sodium flame photometry",
-        "sodium flame photometry",
-        "potassium potentiometrically",
-        "potassium potentiometrically",
-        "platelet count - Electronic",
-        "cefalinetijd - coagulation",
-        "Current ph - PCO2 - stand.bicarbonaat",
-        "crossmatch methods, three fully-",
-        "crossmatch methods, three fully-",
-        "lactic acid enzymatic",
-        "lactic acid enzymatic",
-        "calcium",
-        "calcium",
-        "O2 saturation",
-        "O2 saturation",
-        "prothrombin",
-        "order rate",
-        "order rate",
-        "190205 Class 3b A205",
-        "190101 reg.toesl above. A101",
-        "filtered red cells",
-        "demurrage - all spec.beh.kinderg.-Reval.",
-        "assumption laboratory",
-        "assumption laboratory",
-        "urea",
-        "hemoglobin photoelectric",
-        "creatinine",
-        "sodium flame photometry",
-        "potassium potentiometrically",
-        "SGOT - Asat kinetic",
-        "SGPT - alat kinetic",
-        "leukocyte count electronic",
-        "platelet count - Electronic",
-        "gammaglutamyltranspeptidase",
-        "CRP c-reactive protein",
-        "order rate",
-        "190205 Class 3b A205",
-        "190101 reg.toesl above. A101",
-        "demurrage - all spec.beh.kinderg.-Reval.",
-        "190205 Class 3b A205",
-        "190101 reg.toesl above. A101",
-        "demurrage - all spec.beh.kinderg.-Reval.",
-        "assumption laboratory",
-        "assumption laboratory",
-        "hemoglobin photoelectric",
-        "creatinine",
-        "sodium flame photometry",
-        "potassium potentiometrically",
-        "order rate",
-        "190205 Class 3b A205",
-        "190101 reg.toesl above. A101",
-        "demurrage - all spec.beh.kinderg.-Reval.",
-        "190205 Class 3b A205",
-        "190101 reg.toesl above. A101",
-        "demurrage - all spec.beh.kinderg.-Reval.",
-        "190205 Class 3b A205",
-        "190101 reg.toesl above. A101",
-        "demurrage - all spec.beh.kinderg.-Reval.",
-        "190205 Class 3b A205",
-        "190101 reg.toesl above. A101",
-        "demurrage - all spec.beh.kinderg.-Reval.",
-        "190205 Class 3b A205",
-        "190101 reg.toesl above. A101",
-        "demurrage - all spec.beh.kinderg.-Reval.",
-        "190205 Class 3b A205",
-        "190101 reg.toesl above. A101",
-        "telephone consultation",
-    )
-    s2 = (
-        "demurrage - all spec.beh.kinderg.-Reval.",
-        "assumption laboratory",
-        "assumption laboratory",
-        "assumption laboratory",
-        "unconjugated bilirubin",
-        "bilirubin - total",
-        "glucose",
-        "urea",
-        "hemoglobin photoelectric",
-        "creatinine",
-        "alkaline phosphatase-kinetic-",
-        "sodium flame photometry",
-        "potassium potentiometrically",
-        "SGOT - Asat kinetic",
-        "SGPT - alat kinetic",
-        "Milk acid dehydrogenase LDH kinetic",
-        "ABO blood group and rhesus factor",
-        "rhesus factor d - Centrifuge method - email",
-        "leukocyte count electronic",
-        "platelet count - Electronic",
-        "gammaglutamyltranspeptidase",
-        "CEA - tumor marker using meia",
-        "squamous cell carcinoma using eia",
-        "calcium",
-        "albumin",
-        "red cell antibody screening",
-        "ca-125 using meia",
-        "order rate",
-        "order rate",
-        "ct abdomen",
-        "First outpatient consultation",
-        "interc.consult clinical anesthesia",
-        "administrative fee - the first pol",
-        "190021 clinical admission A002",
-        "190205 Class 3b A205",
-        "190101 reg.toesl above. A101",
-        "demurrage - all spec.beh.kinderg.-Reval.",
-        "cytology - lymfeklierpuncti",
-        "Upper abdominal ultrasound",
-        "ro gel. as ass. to puncture - biopsy l",
-        "190205 Class 3b A205",
-        "190101 reg.toesl above. A101",
-        "demurrage - all spec.beh.kinderg.-Reval.",
-        "190205 Class 3b A205",
-        "190101 reg.toesl above. A101",
-        "demurrage - all spec.beh.kinderg.-Reval.",
-        "190205 Class 3b A205",
-        "190101 reg.toesl above. A101",
-        "demurrage - all spec.beh.kinderg.-Reval.",
-        "190205 Class 3b A205",
-        "190101 reg.toesl above. A101",
-        "demurrage - all spec.beh.kinderg.-Reval.",
-        "190205 Class 3b A205",
-        "190101 reg.toesl above. A101",
-        "telephone consultation",
-    )
+'''
