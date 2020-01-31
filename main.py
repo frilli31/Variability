289d2046844157351fca443e22baff9282bca442
luca <luca.all1996@gmail.com>
2020-01-31 12:01:35 +0100

    Fixes

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from pm4py.objects.log.importer.xes import factory as xes_import_factory
 from variability import *
 
 if __name__ == "__main__":
-    log_path = 'resources/L3.xes'
+    log_path = 'resources/L16.xes'
     log = xes_import_factory.apply(log_path)
     print(Counter(tuple(event["concept:name"] for event in case) for case in log))
+    print(len(log))
     print(compute_variant_variability(log))
     print(compute_edit_distance_variability(log))
     print(compute_my_variability(log))
